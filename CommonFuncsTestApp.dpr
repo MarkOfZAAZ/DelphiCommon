2c8fa624b047ac1c1a6bc2ebe35ec6acf48fdb90
Mark Richards <zaazapps@gmail.com>
2025-10-23 18:08:46 +0100

    Renamed the application as was Funs not Funcs

---
+++ CommonFuncsTestApp.dpr
@@ -0,0 +1,28 @@
+// -----------------------------------------------------------------------------
+// Copyright © 1994 - 2025 Aldwicks Limited
+//
+// Last changed: 22.10.2025 10:23
+// -----------------------------------------------------------------------------
+
+program CommonFuncsTestApp;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  Unit37 in 'Unit37.pas' {Form37},
+  uCommonDialogs in 'uCommonDialogs.pas',
+  uHelperTabControl in 'uHelperTabControl.pas',
+  uCommonPDF in 'uCommonPDF.pas',
+  uCommonSQLLite in 'uCommonSQLLite.pas',
+  uCommonMySQL in 'uCommonMySQL.pas',
+  uCommonAppversion in 'uCommonAppversion.pas',
+  uHelperListView in 'uHelperListView.pas',
+  uHelperStrings in 'uHelperStrings.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm37, Form37);
+  Application.Run;
+end.
